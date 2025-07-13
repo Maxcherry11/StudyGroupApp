ec8d551238d26ee887799b790f6a24796294b6e9
Maxcherry11 <billowy.doubler.3v@icloud.com>
2025-07-13 15:28:43 -0500

    Add 12 Week Year tab placeholders

---
+++ StudyGroupApp/TwelveWeekYearView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct TwelveWeekYearView: View {
+    var body: some View {
+        NavigationStack {
+            Text("12 Week Year (Coming Soon)")
+                .font(.title)
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    TwelveWeekYearView()
+}
