cd42d80ad896fb5a10a43609629813c466952bc0
Maxcherry11 <billowy.doubler.3v@icloud.com>
2025-07-20 19:16:24 -0500

    Make GoalProgress Codable and Equatable

--- StudyGroupApp/TeamModels.swift
+++ StudyGroupApp/TeamModels.swift
@@ -1,10 +1,16 @@
 import Foundation
 
-struct GoalProgress: Identifiable {
-    let id = UUID()
+struct GoalProgress: Identifiable, Codable, Equatable {
+    /// Stable identifier for each goal. Defaults to a new UUID when not provided.
+    let id: UUID
     var title: String
     var percent: Double
-}
 
-extension GoalProgress: Equatable {}
+    /// Creates a new `GoalProgress` optionally overriding the generated `id`.
+    init(id: UUID = UUID(), title: String, percent: Double) {
+        self.id = id
+        self.title = title
+        self.percent = percent
+    }
+}
 
